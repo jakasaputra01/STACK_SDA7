8385964195cb5d821011c8bb545d4591fea997e2
jakasaputra01 <105882316+jakasaputra01@users.noreply.github.com>
2022-06-07 17:40:13 +0700

    Create Stack.cpp

---
+++ Stack.cpp
@@ -0,0 +1,87 @@
+//Nama  : Jaka Saputra
+//NPM   : G1A016020
+//Kelas : Informatika-B
+
+#include <iostream>
+#define MAX 100 //nilai maksimum data stack
+using namespace std;
+
+struct Stack {
+    int puncak; 
+	int data[MAX];
+} S;
+
+void init() {
+    S.puncak = -1;
+}
+ 
+bool isEmpty() {
+    return S.puncak == -1;
+}
+ 
+bool isFull() {
+    return S.puncak == MAX -1;
+}
+ 
+void push() {
+    if (isFull()) {
+        cout << "\nStack Penuh" << endl;
+    } else {
+        S.puncak++;
+        cout << "\nMasukkan data : ";
+        cin >> S.data[S.puncak];
+        cout << "\n Data : '" << S.data[S.puncak] <<endl; 
+		cout << "Data Ditambahkan"<< endl;
+    }
+}
+ 
+void pop() {
+    if (isEmpty()) {
+        cout << "\nStack Is Empty !! " << endl;
+    } else {
+        cout << "\n Data :"<<S.data[S.puncak] <<endl;
+        cout << "Data Dihapus" << endl;
+        S.puncak--;
+    }
+}
+ 
+void printStack() {
+    if (isEmpty()) {
+        cout<< "\nStack Kosong\n";
+    } else {
+        cout << "Data Stack : ";
+        for (int i = S.puncak; i >= 0; i--)
+            cout << S.data[i] <<  ( ",");
+    }
+}
+
+int main() {
+	int choose;
+	do
+	{
+		cout << "--------------STACK-------------\n"
+			<< "---------------------------------\n"
+			<< " 1. Menambahkan Data  \n"
+			<< " 2. Menghapus Data \n"
+			<< " 3. Menampilkan Data \n"
+			<< " 4. EXIT \n\n"
+			<< "----------------------------------\n"
+			<< "Masukkan pilihan : "; cin >> choose;
+		switch (choose)
+		{
+		case 1:
+			push();
+			break;
+		case 2:
+			pop();
+			break;
+		case 3:
+			printStack();
+			break;
+		default:
+			cout << "Pilihan tidak tersedia";
+			break;
+		}
+	} while (choose !=3);
+	return 0;
+}
